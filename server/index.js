b14c1ca41c45518b14ee20764288ed541c0d99c3
harshy261 <iyadavharsh261@gmail.com>
2024-08-07 13:05:22 +0530

    Port Change

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const fileUpload = require("express-fileupload");
 const dotenv = require("dotenv");
 
 // Setting up port number
-const PORT = process.env.PORT || 4000;
+const port = process.env.PORT || 4000;
 
 // Loading environment variables from .env file
 dotenv.config();
@@ -57,8 +57,8 @@ app.get("/", (req, res) => {
 });
 
 // Listening to the server
-app.listen(PORT, () => {
-	console.log(`App is listening at ${PORT}`);
+app.listen(port, () => {
+	console.log(`App is listening at ${port}`);
 });
 
 // End of code.
